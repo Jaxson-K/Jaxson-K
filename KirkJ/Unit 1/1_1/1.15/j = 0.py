67317d25c99ebb4b234e63d89aed86f56146fc15
Jaxson-K <91694295+Jaxson-K@users.noreply.github.com>
2021-10-27 11:08:07 -0700

    Delete KirkJ directory

--- KirkJ/Unit 1/1_1/1.15/j = 0.py
+++
@@ -1,8 +0,0 @@
-j = 0
-while j < 3:
-   j = j + 1
-   print("j: ", j)
-   i = 0
-   while i <= 3:
-      i = i + 1
-      print("i: ", i)
--- KirkJ/gongcha = 10.py
+++
@@ -1,8 +0,0 @@
-gongcha = 10
-happy_lemon = 0
-while happy_lemon < gongcha:
-  gongcha -= 1
-  happy_lemon +=1
-
-print("Gongcha:",gongcha)
-print("happy_lemon:",happy_lemon)
