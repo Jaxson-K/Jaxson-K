609fcb1d32a7b18809034a4ebb576d7ba1ec4511
Jaxson-K <91694295+Jaxson-K@users.noreply.github.com>
2021-10-27 10:23:05 -0700

    Add files via upload

---
+++ KirkJ/Unit 1/1_1/1.15/xy.py
@@ -0,0 +1,16 @@
+
+# import package
+import turtle
+  
+  
+# screen object
+wn = turtle.Screen()
+  
+# method to perform action
+def fxn(x, y):
+  turtle.goto(x, y)
+  turtle.write(str(x)+","+str(y))
+  
+# onclick action 
+wn.onclick(fxn)
+wn.mainloop()
